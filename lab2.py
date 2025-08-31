39e8c586918571104bdb4828cd477e8567af7b5f
leycraft <leycraft@hotmail.com>
2025-08-31 16:08:15 +0700

    week4 update

--- lab2.py
+++ lab2.py
@@ -14,20 +14,29 @@ class App:
         pygame.init()
         self.screen = pygame.display.set_mode((window_width, window_height))
         self.clock = pygame.time.Clock()
-
+        self.CHANGE_DIR = pygame.USEREVENT +1
+        pygame.time.set_timer(self.CHANGE_DIR, 2000)
         self.running = True
 
         self.ball = Agent(position = Vector2(window_width/2, window_height/2), 
                           radius = 100, 
                           color = (100,0,0))
+        
+        self.target = Vector2(0, 0)
+
 
     def handle_input(self):
-         for event in pygame.event.get():
-                    if event.type == pygame.QUIT:
-                        self.running = False
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+
+        mouse_x, mouse_y = pygame.mouse.get_pos()
+        self.target = Vector2(mouse_x, mouse_y)
 
     def update(self, delta_time_ms):
+        self.ball.seek_to(self.target)
         self.ball.update(delta_time_ms)
+        
     
     def draw(self):
         self.screen.fill("gray")
