7446dbefafb11416da9f657ac05c7677fd7e94ca
leycraft <leycraft@hotmail.com>
2025-09-26 00:30:52 +0700

    homing missile homework

---
+++ Sumranchit_Pattanut_Missile/Sumranchit_Pattanut_Missile.py
@@ -0,0 +1,162 @@
+#lab2
+import pygame
+from pygame.draw import circle, line, rect
+from pygame.math import Vector2
+from pygame import image
+import random
+import math
+
+from missile import Missile
+
+window_width = 1280
+window_height = 720
+
+class App:
+    def __init__(self):
+        print("Application is created.")
+        pygame.init()
+        self.screen = pygame.display.set_mode((window_width, window_height))
+        self.clock = pygame.time.Clock()
+        self.CHANGE_DIR = pygame.USEREVENT +1
+        pygame.time.set_timer(self.CHANGE_DIR, 2000)
+        self.running = True
+
+        self.missile_position = Vector2(window_width/2, 600)
+
+        
+        self.missiles = [
+            Missile(position = self.missile_position, 
+                    sprite_location = "sprites\\missile.png"),
+            Missile(position = self.missile_position, 
+                    sprite_location = "sprites\\missile.png"),
+            Missile(position = self.missile_position, 
+                    sprite_location = "sprites\\missile.png"),
+            Missile(position = self.missile_position, 
+                    sprite_location = "sprites\\missile.png"),
+            Missile(position = self.missile_position, 
+                    sprite_location = "sprites\\missile.png"),
+            Missile(position = self.missile_position, 
+                    sprite_location = "sprites\\missile.png"),
+            Missile(position = self.missile_position, 
+                    sprite_location = "sprites\\missile.png"),
+            Missile(position = self.missile_position, 
+                    sprite_location = "sprites\\missile.png"),
+            Missile(position = self.missile_position, 
+                    sprite_location = "sprites\\missile.png"),
+            Missile(position = self.missile_position, 
+                    sprite_location = "sprites\\missile.png"),
+            Missile(position = self.missile_position, 
+                    sprite_location = "sprites\\missile.png"),
+            Missile(position = self.missile_position, 
+                    sprite_location = "sprites\\missile.png"),
+            Missile(position = self.missile_position, 
+                    sprite_location = "sprites\\missile.png"),
+            Missile(position = self.missile_position, 
+                    sprite_location = "sprites\\missile.png"),
+            Missile(position = self.missile_position, 
+                    sprite_location = "sprites\\missile.png"),
+            Missile(position = self.missile_position, 
+                    sprite_location = "sprites\\missile.png"),
+            Missile(position = self.missile_position, 
+                    sprite_location = "sprites\\missile.png"),
+            Missile(position = self.missile_position, 
+                    sprite_location = "sprites\\missile.png"),
+            Missile(position = self.missile_position, 
+                    sprite_location = "sprites\\missile.png"),
+            Missile(position = self.missile_position, 
+                    sprite_location = "sprites\\missile.png")
+        ]
+
+        self.target = Vector2(0, 0)
+
+        self.missile_counter = 0
+
+        self.barrel = "sprites\\barrel.png"
+        self.barrel_image = image.load(self.barrel).convert_alpha()
+        self.barrel_rect = self.barrel_image.get_rect(center = self.missile_position)
+        self.barrel_rotation = 0
+        self.barrel_rotation_increment = 0.5
+
+        self.rotated_barrel = 0
+
+        # decorations
+        self.bg = "sprites\\sky.png"
+        self.dome = "sprites\\dome.png"
+
+        self.bg_image = image.load(self.bg).convert_alpha()
+        self.dome_image = image.load(self.dome).convert_alpha()
+
+
+
+    def handle_input(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    if self.missiles[self.missile_counter].is_active == False:
+                        self.missiles[self.missile_counter].enable_missile(self.barrel_rotation)
+
+                    
+                        self.missile_counter += 1
+                        if self.missile_counter >= len(self.missiles):
+                            self.missile_counter = 0
+
+
+        mouse_x, mouse_y = pygame.mouse.get_pos()
+        self.target = Vector2(mouse_x, mouse_y)
+
+    def update(self, delta_time_s):
+
+        for missile in self.missiles:
+            missile.missile_AI(self.target)
+            missile.update(delta_time_s)
+
+        # barrel rotation
+        self.rotated_barrel = pygame.transform.rotate(self.barrel_image, self.barrel_rotation)
+        self.barrel_rect = self.rotated_barrel.get_rect(center = self.missile_position)
+
+        self.barrel_rotation += self.barrel_rotation_increment
+
+        if self.barrel_rotation > 180:
+            self.barrel_rotation_increment = -abs(self.barrel_rotation_increment)
+        elif self.barrel_rotation < 0:
+            self.barrel_rotation_increment = abs(self.barrel_rotation_increment)
+
+    
+    def draw(self):
+        self.screen.fill("gray")
+
+        self.screen.blit(self.bg_image, Vector2(0,0))
+
+        for missile in self.missiles:
+            missile.draw(self.screen)
+
+        
+
+        self.screen.blit(self.rotated_barrel, self.barrel_rect)
+
+        self.screen.blit(self.dome_image, Vector2(window_width/2 - (self.dome_image.get_width() / 2), 500))
+
+        circle(self.screen, "Red", self.target, 20)
+
+        pygame.display.flip()
+    
+
+    def run(self):
+        while self.running:
+            dt = self.clock.tick(60)
+            self.handle_input()
+            self.update(dt)
+            self.draw()
+            
+
+        pygame.quit()
+
+
+def main():
+    app = App()
+    app.run()
+
+if __name__ == "__main__":
+    main()
---
+++ Sumranchit_Pattanut_Missile/missile.py
@@ -0,0 +1,143 @@
+from pygame.math import Vector2
+from pygame.draw import circle, line, rect
+from pygame import image
+
+import math
+import pygame
+
+class Missile:
+    def __init__(self, position, sprite_location):
+        self.position = position
+        self.storage_position = position
+
+        self.vel = Vector2(0, 0)
+        self.acc = Vector2(0, 0)
+        self.mass = 1.0
+        self.STOP_DIST = 5
+
+        self.timer = 0
+        self.is_active = False
+
+         # sprite
+        self.sprite_image = image.load(sprite_location).convert_alpha()
+        self.sprite_width = self.sprite_image.get_width()
+        self.sprite_height = self.sprite_image.get_height()
+        self.sprite_position = position - Vector2(self.sprite_image.get_width()/2, self.sprite_image.get_height()/2)
+
+        self.first_shot_angle = 0
+        self.sprite_rotation = 0
+        self.rotated_image = 0
+
+
+    def missile_tracking(self, target_pos):
+        MAX_FORCE = 5
+        d = target_pos - self.position
+        
+        dist = d.length()
+
+        if dist < self.STOP_DIST:
+            self.reset_state()
+
+        elif self.is_active == True:
+            desired = d.normalize() * MAX_FORCE
+
+            steering = desired - self.vel
+
+            # set missile rotation
+            self.sprite_rotation = 360 - self.calculate_angle(self.position, target_pos)
+            
+            if steering.length() > MAX_FORCE:
+                steering.scale_to_length(MAX_FORCE)
+
+            self.apply_force(steering)   
+
+    def missile_nudge(self, target_pos):
+        MAX_FORCE = 1
+        d = target_pos - self.position
+        
+        dist = d.length()
+
+        if dist < self.STOP_DIST:
+            self.reset_state()
+
+        elif self.is_active == True:
+            desired = d.normalize() * MAX_FORCE * (dist / 5000)
+
+            steering = desired
+
+            # set missile rotation
+            self.sprite_rotation = 360 - self.calculate_angle(self.position, target_pos)
+            
+            if steering.length() > MAX_FORCE:
+                steering.scale_to_length(MAX_FORCE)
+
+            self.apply_force(steering) 
+
+
+    def go_straight(self):
+        self.vel = self.first_shot_angle * 5
+        self.sprite_rotation = 360 - self.calculate_angle(Vector2(0, 0), self.first_shot_angle)
+
+    
+    def missile_AI(self, target_pos):
+        if self.is_active == True:
+            self.timer += 1
+
+            if self.timer > 150:
+                self.missile_tracking(target_pos)
+            elif self.timer > 50:
+                self.missile_nudge(target_pos)
+            else:
+                self.go_straight()
+
+    def apply_force(self, force):
+        self.acc += force / self.mass
+
+    def enable_missile(self, shot_angle):
+        self.is_active = True
+
+        # change shot angle to vector
+        angle_radians = math.radians(shot_angle)
+        self.first_shot_angle = Vector2(math.cos(angle_radians), -math.sin(angle_radians))
+
+    def reset_state(self):
+        self.acc = Vector2(0,0)
+        self.vel = Vector2(0,0)
+        self.position = self.storage_position
+        self.timer = 0
+        self.is_active = False
+
+
+    def calculate_angle(self, p1, p2):
+        x1, y1 = p1
+        x2, y2 = p2
+
+        dx = x2 - x1
+        dy = y2 - y1
+
+        # Calculate the angle in radians using atan2
+        angle_radians = math.atan2(dy, dx)
+
+        # Convert the angle to degrees
+        angle_degrees = math.degrees(angle_radians)
+
+        # Ensure the angle is positive (0 to 360 degrees)
+        if angle_degrees < 0:
+            angle_degrees += 360
+
+        return angle_degrees
+
+    def update(self, delta_time_ms):
+        self.vel = self.vel + self.acc
+        self.position = self.position + self.vel
+        self.acc = Vector2(0,0)
+
+        # update rotation
+
+        self.rotated_image = pygame.transform.rotate(self.sprite_image, self.sprite_rotation)
+
+        self.sprite_position = self.position - Vector2(self.rotated_image.get_width()/2, self.rotated_image.get_height()/2)
+
+
+    def draw(self, screen):
+        screen.blit(self.rotated_image, self.sprite_position)
